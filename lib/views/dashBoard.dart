1ee217e428e3c2d875fedb0b326b6fdd7b539a53
MOHAMED KAMAL <87618925+Mk-OTHMAN@users.noreply.github.com>
2024-04-29 21:05:57 +0300

    test 3 commit

--- lib/views/dashBoard.dart
+++ lib/views/dashBoard.dart
@@ -105,7 +105,7 @@ class DashBoard extends StatelessWidget {
                               buttonNameFontSize: 5,
                               color: Colors.orange,
                             ),
-                            //! some space height ___________
+                            //! some space height ____________
                             Functions.someSpaceHeight(25),
                             // !____ stock elevated button ______
                             CustomElevatedButton(
